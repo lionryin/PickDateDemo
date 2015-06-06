23d5910ceb2b27aeb38834fbd7f0529354a7b9d4
lionryin <710523800@qq.com>
2015-06-06 16:05:05 +0800

    [lx]init commit

--- PickDateDemo/ViewController.m
+++ PickDateDemo/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *dateTF;
+@property (strong, nonatomic) UIDatePicker *pick;
 
 @end
 
@@ -22,6 +24,39 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)selectDoneAction{
+    [_dateTF resignFirstResponder];
+    
+    NSDateFormatter *formatter=[[NSDateFormatter alloc]init];
+    formatter.dateFormat=@"yy-MM-dd HH:mm:ss";
+    NSString *timeStr=[formatter stringFromDate:_pick.date];
+    
+    
+    _dateTF.text = timeStr;
+}
+
+#pragma mark - UITextFieldDelegate
+- (void)textFieldDidBeginEditing:(UITextField *)textField{
+    _pick = [[UIDatePicker alloc] init];
+    
+    _dateTF.inputView = _pick;
+    
+    UIToolbar *accessoryView = [[UIToolbar alloc] init];
+    accessoryView.frame = CGRectMake(0, 0, self.view.frame.size.width, 38);
+    accessoryView.backgroundColor = [UIColor grayColor];
+    
+    UIBarButtonItem *doneBtn = [[UIBarButtonItem alloc] initWithTitle:@"完成" style:UIBarButtonItemStylePlain target:self action:@selector(selectDoneAction)];
+    [doneBtn setTintColor:[UIColor blueColor]];
+    
+   UIBarButtonItem *spaceBtn=[[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    accessoryView.items=@[spaceBtn,doneBtn];
+    _dateTF.inputAccessoryView = accessoryView;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField{
+    
 }
 
 @end
